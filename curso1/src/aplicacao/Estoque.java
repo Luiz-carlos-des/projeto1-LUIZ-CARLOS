ddfc14f54686ee135ecdc9bd6bc5be5452485e90
LUIZ CARLOS <zinhosantos@gmail.com>
2026-02-16 14:19:01 -0300

    iniciando um controle de estoque

--- curso1/src/aplicacao/Estoque.java
+++ curso1/src/aplicacao/Estoque.java
@@ -25,7 +25,27 @@ public class Estoque {
 				
 		produto.quantidade = sc.nextInt();
 		
-		System.out.println(produto);
+		System.out.println();
+		
+		
+		System.out.println("Dados do produto: " + produto);
+		
+		System.out.println();
+		
+		System.out.println("Informe a quantidade a ser incluida no estoque: ");
+		int quantidade = sc.nextInt();
+		produto.incluirEmEstoque(quantidade);
+		
+		System.out.println();
+		System.out.println("Dados atualizados: " + produto);
+		
+		System.out.println();
+		System.out.println("Informe a quantidade a ser excluida do estoque: ");
+		quantidade = sc.nextInt();
+		produto.excluirDoEstoque(quantidade);
+		
+		System.out.println();
+		System.out.println("Dados atualizados: " + produto);
 		
 		
 		
